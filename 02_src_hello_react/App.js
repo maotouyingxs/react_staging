5d182b55306f2544c5d6a3356dd680b6f4155c10
maotouyingxs <renslc@163.com>
2021-05-22 14:10:08 +0800

    hello_react

--- 02_src_hello_react/App.js
+++ 02_src_hello_react/App.js
@@ -1,13 +1,16 @@
 // 创建外壳组件
 import React, { Component } from 'react'
 
+import Hello from './components/Hello/Hello'
+import Welcom from './components/Welcome/Welcome'
 
 // 创建并暴露 App 组件
 export default class App extends Component {
     render() {
         return (
             <div>
-                
+                <Hello />
+                <Welcom />
             </div>
         )
     }
