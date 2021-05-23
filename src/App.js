527f4408514ffa6221b0dc4298f3be40564b42b0
maotouyingxs <renslc@163.com>
2021-05-23 17:49:30 +0800

    准备工作

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 // 创建外壳组件
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
 
 // 创建并暴露 App 组件
 export default class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Search />
-                <List />
+            <div>
+                App...
             </div>
         )
     }
