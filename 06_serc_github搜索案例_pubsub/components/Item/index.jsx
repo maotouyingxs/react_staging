308457dcd37270c47253eac8c1107337ef986a20
maotouyingxs <renslc@163.com>
2021-05-23 17:48:26 +0800

    整理

--- 06_serc_github搜索案例_pubsub/components/Item/index.jsx
+++ 06_serc_github搜索案例_pubsub/components/Item/index.jsx
@@ -4,7 +4,7 @@ export default class Item extends Component {
     render() {
         const { id, avatar_url, login, html_url } = this.props.user
         return (
-            <div key={id} className="card">
+            <div className="card">
                 <a href={html_url} target="_blank" rel="noreferrer">
                     <img alt="head_protrait" src={avatar_url} style={{ width: '100px' }} />
                 </a>
