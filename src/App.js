2704f13c57dd53aef8376478d3d0bcc1a38bed40
maotouyingxs <renslc@163.com>
2021-05-23 22:07:25 +0800

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // 创建外壳组件
 import React, { Component } from 'react'
-import { Link, BrowserRouter, Route } from 'react-router-dom'
+import { NavLink, BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/Home'  // Home 是路由组件
 import About from './pages/About'  // About 是路由组件
 import Header from './components/Header' // Header 是一般组件
@@ -21,8 +21,8 @@ export default class App extends Component {
                             {/* <a className="list-group-item" href="./about.html">About</a>
                             <a className="list-group-item active" href="./home.html">Home</a> */}
                             {/* 在 React 中靠路由链接实现切换组件 -- 编写路由链接*/}
-                            <Link className="list-group-item" to="/about" >About</Link>
-                            <Link className="list-group-item" to="/home" >Home</Link>
+                            <NavLink activeClassName="demo" className="list-group-item" to="/about" >About</NavLink>
+                            <NavLink activeClassName="demo" className="list-group-item" to="/home" >Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
